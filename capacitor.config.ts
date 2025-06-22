25c851557dc2f55d030d8ac5821884e6d621f4f9
Elvin-Provility <elvin@provility.com>
2025-06-22 19:53:17 +0530

    Setup ionic project

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,12 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'ionic-app',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
 };
 
 export default config;
